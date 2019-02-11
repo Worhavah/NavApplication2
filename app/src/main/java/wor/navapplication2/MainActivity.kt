4f2a9a0e62cac8f5dce32b7a6e5e21ae2cdda1ea
Worhavah <473097512@qq.com>
2019-02-12 00:11:47 +0800

    aaaaaaaaaaaaaaa

--- app/src/main/java/wor/navapplication2/MainActivity.kt
+++ app/src/main/java/wor/navapplication2/MainActivity.kt
@@ -1,12 +1,17 @@
 package wor.navapplication2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 
+//https://www.jianshu.com/p/ad040aab0e66 source
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp() =
+        findNavController(this, R.id.my_nav_host_fragment).navigateUp()
 }
---
+++ app/src/main/java/wor/navapplication2/MainPage1Fragment.kt
@@ -0,0 +1,25 @@
+package wor.navapplication2
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main_page1.*
+
+class MainPage1Fragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_main_page1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_page2)
+        }
+    }
+
+}
